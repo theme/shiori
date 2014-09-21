17840b4fd55196cbac1df99f1298f4d97767705e
theme <xenotheme@gmail.com>
2014-09-21 20:07:48 +0800

    load bookmarks onload

--- src/js/arranger.js
+++ src/js/arranger.js
@@ -1,82 +1,98 @@
 (function() {
-  var dumpBookmarks, dumpNode, dumpTreeNodes, listBookmarks, searchBookmarks;
+    var dumpBookmarks, dumpNode, dumpTreeNodes, listBookmarks, searchBookmarks;
 
-  $(function() {
-    $('#search-button').click(function() {
-      searchBookmarks();
-      event.preventDefault();
-    });
-    $('#search-form').submit(function() {
-      searchBookmarks();
-      event.preventDefault();
-    });
-    listBookmarks('bookmarks');
-  });
-
-  listBookmarks = function(divName) {
-    var bookmarkTreeNodes;
-    bookmarkTreeNodes = chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
-      return $('#' + divName).append(dumpTreeNodes(bookmarkTreeNodes, null));
-    });
-  };
-
-  searchBookmarks = function() {
-    console.log('searchBookmarks()');
-    if ($('#search-text').val()) {
-      $('#search-results').empty();
-      $('#panel-search-results').removeClass("hidden");
-      dumpBookmarks($('#search-text').val());
-    } else {
-      $('#search-results').empty();
-      $('#panel-search-results').addClass("hidden");
-    }
-  };
-
-  dumpBookmarks = function(query) {
-    var bookmarkTreeNodes;
-    bookmarkTreeNodes = chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
-      $('#search-results').append(dumpTreeNodes(bookmarkTreeNodes, query));
-    });
-  };
-
-  dumpTreeNodes = function(bookmarkNodes, query) {
-    var list, node, _i, _len;
-    list = $('<ul>');
-    for (_i = 0, _len = bookmarkNodes.length; _i < _len; _i++) {
-      node = bookmarkNodes[_i];
-      if (!node.url){   // is folder
-          list.append(dumpNode(node, query));
-      }
-    }
-    return list;
-  };
-
-  dumpNode = function(bookmarkNode, query) {
-    var anchor, folder, li, span;
-    if (bookmarkNode.title) {
-      if (query && !bookmarkNode.children) {
-        if (String(bookmarkNode.title).indexOf(query) === -1) {
-          return $('<span></span>');
+    document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+            var sbtn = $('#search-button');
+            sbtn.addEventListener('click', (function(e) {
+                searchBookmarks();
+                e.preventDefault();
+            }));
+            var sform = $('#search-form');
+            sform.addEventListener('submit',(function(e) {
+                searchBookmarks();
+                e.preventDefault();
+            }));
+            listBookmarks('bookmarks');
         }
-      }
-      span = $('<span>');
-      if (bookmarkNode.url) {
-        anchor = $('<a>');
-        anchor.attr('href', bookmarkNode.url);
-        anchor.attr('target', "_blank");
-        anchor.text('[' + bookmarkNode.id + '/' + bookmarkNode.index + ']' + bookmarkNode.title);
-        span.append(anchor);
-      } else {
-        folder = $('<p>');
-        folder.text('[' + bookmarkNode.id + '/' + bookmarkNode.index + '/' + bookmarkNode.title + ']');
-        span.append(folder);
-      }
-    }
-    li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
-    if (bookmarkNode.children && bookmarkNode.children.length > 0) {
-      li.append(dumpTreeNodes(bookmarkNode.children, query));
-    }
-    return li;
-  };
+    };
+
+    listBookmarks = function(divName) {
+        console.log('listBookmarks()');
+        var bookmarkTreeNodes, bmlist;
+        bookmarkTreeNodes = chrome.bookmarks.getTree(function(bmt) {
+            $('#' + divName).appendChild(dumpTreeNodes(bmt, null));
+        });
+    };
+
+    searchBookmarks = function() {
+        console.log('searchBookmarks()');
+        if ($('#search-text').value) {
+            var results = $('#search-results')
+            while( results.firstChild ){
+                results.removeChild(result.firstChild);
+            }
+            var panelresults = $('#panel-search-results');
+            panelresults.className.replace(/(?:^|\s)hidden(?!\S)/g,'');
+            dumpBookmarks($('#search-text').value);
+        } else {
+            var results = $('#search-results')
+            while( results.firstChild ){
+                results.removeChild(result.firstChild);
+            }
+            var panelresults = $('#panel-search-results');
+            panelresults.className += "hidden";
+        }
+    };
+
+    dumpBookmarks = function(query) {
+        console.log('dumpBookmarks()');
+        var bookmarkTreeNodes;
+        bookmarkTreeNodes = chrome.bookmarks.getTree(function(bmt) {
+            $('#search-results').appendChild(dumpTreeNodes(bmt, query));
+        });
+    };
+
+    dumpTreeNodes = function(bookmarkNodes, query) {
+        var list, node, _i, _len;
+        list = $('<ul>');
+        for (_i = 0, _len = bookmarkNodes.length; _i < _len; _i++) {
+            node = bookmarkNodes[_i];
+            if (!node.url){   // is folder
+                list.appendChild(dumpNode(node, query));
+            }
+        }
+        return list;
+    };
+
+    dumpNode = function(bookmarkNode, query) {
+        var anchor, folder, li, span;
+        span = $('<span>');
+        if (bookmarkNode.title) {
+            if (query && !bookmarkNode.children) {
+                if (String(bookmarkNode.title).indexOf(query) === -1) {
+                    return span;
+                }
+            }
+            if (bookmarkNode.url) {
+                anchor = $('<a>');
+                anchor.setAttribute('href', bookmarkNode.url);
+                anchor.setAttribute('target', "_blank");
+                anchor.innerHTML= '[' + bookmarkNode.id + '/' + bookmarkNode.index + ']' + bookmarkNode.title;
+                span.appendChild(anchor);
+            } else {
+                folder = $('<p>');
+                folder.innerHTML = '[' + bookmarkNode.id + '/' + bookmarkNode.index + '/' + bookmarkNode.title + ']';
+                span.appendChild(folder);
+            }
+        }
+
+        li = $(bookmarkNode.title ? '<li>' : '<div>');
+        li.appendChild(span);
+        if (bookmarkNode.children && bookmarkNode.children.length > 0) {
+            li.appendChild(dumpTreeNodes(bookmarkNode.children, query));
+        }
+        return li;
+    };
 
 }).call(this);
--- src/js/util.js
+++ src/js/util.js
@@ -1,4 +1,20 @@
-// $ -> getElementById
+/* $ -> getElementById */
 function $(idOrType){
-    return document.getElementById(idOrType);
-}
+    //console.log('$('+idOrType+')');
+    if( typeof idOrType != 'string' || idOrType.length == 0){
+            return null;
+        }
+    var el;
+    switch ( idOrType.charAt(0) ){
+        case '#':
+            el = document.getElementById(idOrType.substring(1));
+            break;
+        case '<':
+            var t = idOrType.substring(1,idOrType.length-1);
+            el = document.createElement(t);
+            break;
+    }
+    return el;
+};
+
+
