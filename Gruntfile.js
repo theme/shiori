5a2153fbb7e671bc34c4d5d78e5f02a106ccef9b
theme <xenotheme@gmail.com>
2015-06-03 08:48:30 +0800

    new icon. grunt-svg2png can not resize rename.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,18 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         copy: {
-            main:{
-                files: [
-                    { expand:true, cwd:'src/js/',src:['**'], dest:'pub/js/', filter:'isFile'}
-                ]
+            main: {
+                files: [{ expand:true, cwd:'src/js/', src:['**'], dest:'pub/js/', filter:'isFile' }]
             }
         },
         jade: {
             compile: {
-                options: {
-                    pretty: true,
-                    data: {
-                        debug: false
-                    }
-                },
-                files: {
-                    "pub/arranger.html": "src/jade/arranger.jade"
-                }
+                options: { pretty: true, data: { debug: false } },
+                files: { "pub/arranger.html": "src/jade/arranger.jade" }
             }
         },
         less: {
-            development: {
-                options: {
-                    paths: ["src/less"]
-                },
-                files: {
-                }
-            },
+            development: { options: { paths: ["src/less"] }, files: { } },
             production: {
                 options: {
                     paths: ["src/less"],
@@ -37,39 +22,48 @@ module.exports = function(grunt) {
                         //bgColor: 'red'
                     }
                 },
-                files: {
-                    "pub/css/arranger.css": "src/less/arranger.less"
-                }
+                files: { "pub/css/arranger.css": "src/less/arranger.less" }
             }
         },
         coffee: {
             compileBare: {
-                options: {
-                    bare: false
-                },
-                files: {
-                     //'pub/js/arranger.js': ['src/coffee/arranger.coffee']
-                }
-            }
+                options: { bare: false },
+                files: { /*'pub/js/arranger.js': ['src/coffee/arranger.coffee'] */}
+            },
         },
+        svg2png: ( function(){
+            var pngsize = [ 16, 19, 48, 128 ];
+            var svgsize = 128.0;
+            // construct task object for grunt task : svg2png
+            var o = {};
+            pngsize.forEach( function( s ) {
+                o['p'+ s.toString(10)] = {
+                    options: { scale: s/svgsize },
+                    files: [ {
+                        cwd: 'src/svg/',
+                        src: 'leaf-shadow.svg',
+                        dest: 'pub/img/icon' + s.toString() + '.png'
+                    } ]
+                };
+            });
+            return o;
+        })(),
         watch: {
-            scripts: {
+            srouceFiles: {
                 files: ['src/**'],
                 tasks: ['default'],
                 options: {
                     spawn: false,
                     interrupt: true,
                     debounceDelay: 250,
-                    event: ['changed'], //changed, added, deleted, all
-                },
+                    event: ['changed'] //changed, added, deleted, all
+                }
             },
             configFiles: {
                 files: [ 'Gruntfile.js', 'config/*.js' ],
-                options: {
-                    reload: true
-                }
-            },
-        },
+                options: { reload: true }
+            }
+        }
     });
     // Load the plugin that provides the "less" task.
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -77,7 +71,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-svg2png');
 
     // Default task(s).
-    grunt.registerTask('default', ['copy','jade','less','coffee']);
+    grunt.registerTask('default', ['copy','jade','less','coffee','svg2png']);
 };
