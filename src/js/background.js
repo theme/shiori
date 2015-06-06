9e06ee40f92a0aa0acdf525cf131049b7b8fa9d1
theme <xenotheme@gmail.com>
2015-06-06 20:58:16 +0800

    only one bookmarks tab.

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,24 @@
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    var newURL = "chrome://bookmarks/";
-    chrome.tabs.create({ url: newURL });
-});
+function getArrangerUrl() {
+  var url = 'chrome://bookmarks/';
+  return url;
+}
+
+function isArrangerUrl(url) {
+  return url.indexOf(getArrangerUrl()) == 0;
+}
+
+function goToArranger() {
+  chrome.tabs.query({}, function(tabs) {
+    // focus, if bookmarks tab exist.
+    for (var i = 0, tab; tab = tabs[i]; i++) {
+      if (tab.url && isArrangerUrl(tab.url)) {
+        chrome.tabs.update(tab.id, {highlighted: true});
+        return;
+      }
+    }
+    // create, if no bookmarks tab.
+    chrome.tabs.create({url: getArrangerUrl()});
+  });
+}
+
+chrome.browserAction.onClicked.addListener(goToArranger);
