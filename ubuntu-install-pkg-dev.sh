458ee3d13f26860317c5b20d9aff05dd74df4f4f
theme <xenotheme@gmail.com>
2016-11-29 18:01:38 +0800

    add develop pkg install script.

---
+++ ubuntu-install-pkg-dev.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+# Ubuntu 16.04
+
+sudo apt-get install -y nodejs
+
+sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
+
+sudo npm i -g grunt-cli
+
+sudo npm i -g bower
+
+# bower install --save moment
